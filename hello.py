ec972d6bc8304a16e5f7acd677e8bfb2186c8de5
Lucas Tepper <lucas@kryten.nl>
2023-05-01 22:51:57 +0200

    .

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
 from flask import Flask
+import platform
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello NEW World from Flask!</h1>'
+    return '<h1>Hello NEW World from Flask running on ' + platform.node() + '</h1>'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("12345"), debug=True)
